28ce89fb9bc34dff4fcb0fc0ccf2c0dd7038eb44
sekercih <hkkskrc@gmail.com>
2021-06-07 18:27:47 +0300

    reactreview

---
+++ src/layouts/Navi.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button,  Menu, Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import CartSummary from "./CartSummary";
+
+export default function Navi() {
+  return (
+    <div>
+      <Menu inverted>
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+
+          <Menu.Menu position="right">
+            <CartSummary />
+            <Menu.Item>
+              <Button primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
